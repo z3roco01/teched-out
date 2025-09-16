c5f14dd7a5467b9b3cf4d879ef37ac77ffd7ea9e
z3roco01 <zerocool@e.email>
2025-09-16 09:58:33 -0500

    temp energy screen

--- src/client/kotlin/z3roco01/techedout/screen/BatteryScreen.kt
+++ src/client/kotlin/z3roco01/techedout/screen/BatteryScreen.kt
@@ -4,24 +4,30 @@ import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.ingame.HandledScreen
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.text.Text
+import net.minecraft.util.Colors
 import z3roco01.techedout.TechedOut
 
 class BatteryScreen(handler: BatteryScreenHandler, private val playerInv: PlayerInventory, title: Text):
     HandledScreen<BatteryScreenHandler>(handler, playerInv, title) {
     val BG_TEXTURE = TechedOut.mkId("textures/gui/container/battery.png")
+    // store centers so they dont have to be calculated each draw call
+    private var centerX = 0
+    private var centerY = 0
 
     override fun init() {
         super.init()
         // ensure title is centred vertically
         this.titleX = (this.backgroundWidth - textRenderer.getWidth(this.title)) / 2
+        // calculate the middle screen coordinates on the x and y to draw bg in the right spot
+        centerX = (this.width - this.backgroundWidth) / 2
+        centerY = (this.height - this.backgroundHeight) / 2
     }
 
     override fun drawBackground(context: DrawContext, delta: Float, mouseX: Int, mouseY: Int) {
-        playerInv.player.sendMessage(Text.of(handler.blockEntity.energyStorage.amount.toString()))
-        // calculate the middle screen coordinates on the x and y
-        val centerX = (this.width - this.backgroundWidth) / 2
-        val centerY = (this.height - this.backgroundHeight) / 2
-
         // render the background texture to the screen with the center coords
-        context.drawTexture(BG_TEXTURE, centerX, centerY, 0, 0, this.backgroundWidth, this.backgroundHeight)    }
+        context.drawTexture(BG_TEXTURE, centerX, centerY, 0, 0, this.backgroundWidth, this.backgroundHeight)
+
+        // temp way to show power amount
+        context.drawText(textRenderer, handler.blockEntity.getAmount().toString(), centerX+50, centerY+20, (0xFF0000FFu).toInt(), true)
+    }
 }
