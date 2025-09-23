238839d09d4daf12ab4493ec8d93f8b4b6730038
z3roco01 <zerocool@e.email>
2025-09-23 09:24:02 -0500

    fix tags to make them work

--- src/main/kotlin/z3roco01/techedout/blockentity/machines/ElectrolyzerBlockEntity.kt
+++ src/main/kotlin/z3roco01/techedout/blockentity/machines/ElectrolyzerBlockEntity.kt
@@ -5,6 +5,10 @@ import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.inventory.SimpleInventory
 import net.minecraft.item.ItemStack
+import net.minecraft.registry.Registries
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.Identifier
 import net.minecraft.util.collection.DefaultedList
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
@@ -14,6 +18,7 @@ import z3roco01.techedout.blockentity.EnergyStorageBlockEntity
 import z3roco01.techedout.blockentity.TechedOutBlockEntities
 import z3roco01.techedout.recipe.ElectrolyzeRecipe
 import z3roco01.techedout.screen.ElectrolyzerScreenHandler
+import java.security.IdentityScope
 import java.util.Optional
 
 class ElectrolyzerBlockEntity(pos: BlockPos, state: BlockState, tier: Tier):
